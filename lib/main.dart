6f21d74c1c2a427bd5b44c1c58a9648344921eee
Ryoga Komine <yun.mes41@gmail.com>
2020-08-16 22:37:49 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _pageList = [
+    CustomPage(pannelColor: Colors.cyan, title: 'Home'),
+    CustomPage(pannelColor: Colors.green, title: 'Settings'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('サンプル1'),
+      ),
+      body: _pageList[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Setting'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class CustomPage extends StatelessWidget {
+  final Color pannelColor;
+  final String title;
+
+  CustomPage({@required this.pannelColor, @required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleTextStyle = Theme.of(context).textTheme.title;
+    return Container(
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              color: pannelColor,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: titleTextStyle.fontSize,
+                color: titleTextStyle.color,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
